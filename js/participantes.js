2386feab69c1da049064096191b620e40af6779c
DESKTOP-NCMIQHK\Hernan <hernanpocchiola@gmail.com>
2018-04-08 11:22:33 -0300

    Participantes funcionando mejor

--- js/participantes.js
+++ js/participantes.js
@@ -1,5 +1,3 @@
-const TOTALEQUIPOS = 4;
-
 var jsonIntegrantes = JSON.parse($.getJSON({'url': "json/integrantes.json", 'async': false}).responseText);
 var jsonEquipos = JSON.parse($.getJSON({'url': "json/equipos.json", 'async': false}).responseText);
 
@@ -32,56 +30,92 @@ function mostrarIntegrantes(datosEquipos, idEquipo) {
        var id = "integrante" + (i+1);
        $("#muestraIntegrantes").append($("<li></li>").addClass("list-group-item").attr("id",id).text(datosEquipos.equipos[equipoElegido].integrantes[i].nickname));
 }
+return equipoElegido;
 }
 
 function mostrarIntegrantesVacio (){
    for (i = 0; i < 3; i++) {
-       var text = "integrante " + (i+1);
+       var text = "Integrante " + (i+1);
        $("#muestraIntegrantes").append($("<li></li>").addClass("list-group-item").text(text));
    }
 }
 
+function mostrarInformacionVacia (){
+    $("#nombre").text("Nombre: ");
+    $("#apellido").text("Apellido: ");
+    $("#edad").text("Edad: ");
+    $("#favCard").text("Carta Favorita: ");
+    $("#favClass").text("Clase Favorita: ");
+}
 
-function mostrarInformacion (datosEquipo, equipoElegido, datosIntegrantes, integranteElegido){
-    var integrantes = ordenarIntegrantes (datosEquipo, equipoElegido, datosIntegrantes);
+function mostrarInformacion (datosEquipos, equipoElegido, datosIntegrantes, idIntegrante){
+    
+    var integranteElegido;
+    switch (idIntegrante) {
+    case "integrante1":
+        integranteElegido=0;
+        break;
+    case "integrante2":
+        integranteElegido=1;
+        break;
+    case "integrante3":
+        integranteElegido=2;
+        break;
+    }
+    var integrantes = ordenarIntegrantes (datosEquipos, equipoElegido, datosIntegrantes);
     var integrante = integrantes[integranteElegido];
     $("#nombre").text("Nombre: " + integrante.nombre);
-    $("#apellido").text("Apellido: "+ integrante.nickname);
+    $("#apellido").text("Apellido: "+ integrante.apellido);
     $("#edad").text("Edad: " + integrante.edad);
     $("#favCard").text("Carta Favorita: " + integrante.cartaFavorita);
     $("#favClass").text("Clase Favorita: " + integrante.claseFavorita);
+    return integrante.foto;
 }
 
-function ordenarIntegrantes (datosEquipo, equipoElegido, datosIntegrantes) {
-    var integrantes = [];
-    for (j = 0; j < datosEquipo.equipos[equipoElegido].integrantes.length;j++) {
+function ordenarIntegrantes (datosEquipos, equipoElegido, datosIntegrantes) {
+    var arrIntegrantes = [];
+    for (j = 0; j < datosEquipos.equipos[equipoElegido].integrantes.length;j++) {
         for (i = 0; i < datosIntegrantes.integrantes.length; i++) {
-            if (datosEquipo.equipos[equipoElegido].integrantes[j].nickname === datosIntegrantes.integrantes[i].nickname) {
-                integrantes[j] = datosIntegrantes.integrantes[i];
+            if (datosEquipos.equipos[equipoElegido].integrantes[j].nickname === datosIntegrantes.integrantes[i].nickname) {
+                arrIntegrantes[j] = datosIntegrantes.integrantes[i];
             }
         }
     }
-    return integrantes;
+    return arrIntegrantes;
 }
 
+function mostrarIcono(datosEquipos, equipoElegido) {
+   var link = datosEquipos.equipos[equipoElegido].icono;
+   var src = "./imagenes/" + link;
+   $("#equipoLogo").append('<img id="logo" alt="Logo del equipo" src= ' +src+' class="logo"/>');   
+}
+
+function mostrarFoto (foto){
+    var src = "./imagenes/" + foto;
+    $("#integranteFoto").append('<img id="foto" alt="Foto del integrante" src= ' +src+' class="logo"/>');   
+}
 
 function clickEquipo (e) {
     $("#muestraIntegrantes").empty();
+    $("#equipoLogo").empty();
     var id = $(e.target).attr("id");
-    mostrarIntegrantes(jsonEquipos,id);
+    var equipoElegido = mostrarIntegrantes(jsonEquipos,id);
+    mostrarIcono(jsonEquipos,equipoElegido);
+    $("#muestraIntegrantes").children().on("click", {x:equipoElegido},clickIntegrante);
     $(e.target).addClass("equipoCSS");
     $(e.target).siblings().removeClass("equipoCSS");
     }
     
  function clickIntegrante (e) {
-    $("#muestraIntegrantes").empty();
+     $("#integranteFoto").empty();
     var id = $(e.target).attr("id");
-    mostrarIntegrantes(jsonEquipos,id);
+    var foto = mostrarInformacion(jsonEquipos,e.data.x,jsonIntegrantes,id);
+    mostrarFoto (foto);
     $(e.target).addClass("equipoCSS");
     $(e.target).siblings().removeClass("equipoCSS");
  }
 
 $(document).ready(mostrarEquipos(jsonEquipos));
 $(document).ready(mostrarIntegrantesVacio());
+$(document).ready(mostrarInformacionVacia());
 $(document).ready($("#muestraEquipos").children().click(clickEquipo));
-$(document).ready(mostrarInformacion(jsonEquipos,2,jsonIntegrantes,2));
